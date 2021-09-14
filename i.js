384bf693673164a8dc941492aa2b7c8b06938704
vsamaru <vsamaru@gmail.com>
2021-09-14 16:13:32 +0300

    first commit

--- i.js
+++ i.js
@@ -5,7 +5,7 @@ const router = require('./1ll/post')
 async function fetchAndApply(re) {
 
   try {
-
+console.log(re)
     return await router(re.request).catch(err => console.error(err))
   } catch (err) {
  console.error(err)
